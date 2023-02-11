8ef7fcef58f52eeab347f478e8dc4f638469e6d8
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2023-02-12 02:56:40 +0530

    Update LoanApplication.sol

--- LoanApplication.sol
+++ LoanApplication.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.10;
 
 contract LoanApplication {
     // Mapping to store loan details
-    mapping (address => Loan[]) public loans;
+    // mapping (address => Loan[]) public loans;
+    mapping (address => mapping(uint256 => Loan)) public loans;
     mapping (address => uint256) public creditworthiness;
-    
+    mapping (address => uint256) public loancounts;
+    // Mapping to store interest rates
+    mapping (uint256 => mapping (uint256 => uint256)) public interestRates;
+
     // Struct to define loan details
     struct Loan {
         uint256 loanId;
@@ -15,63 +18,79 @@ contract LoanApplication {
         uint256 interestRate;
         uint256 interest;
         bool approved;
+        bool repaid;
     }
 
     uint256 private loanCounter = 0;
     uint256 public contractBalance = 1000;
 
+    // Declare events
+    event LoanApplied(address indexed borrower, uint256 loanId);
+    event LoanApproved(address indexed borrower, uint256 loanId);
+    event LoanRepaid(address indexed borrower, uint256 loanId);
+    event InterestRateSet(uint256 amount, uint256 duration, uint256 interestRate);
+
+    // Generate a unique loan ID
+    function generateLoanId(uint256 _amount) internal view returns (uint256) {
+        uint256 time = block.timestamp;
+        uint256 random = uint256(keccak256(abi.encodePacked(time, msg.sender, _amount)));
+        return random;
+    }
+
     // Function to apply for a loan
     function applyForLoan(uint256 _amount, uint256 _duration) public {
         
         // Set default creditworthiness to 50
-        if ((creditworthiness[msg.sender] == 0) && (loans[msg.sender].length == 0)) {
+        if ((creditworthiness[msg.sender] == 0) && (loancounts[msg.sender] == 0)) {
             creditworthiness[msg.sender] = 50;
         }
 
         require(creditworthiness[msg.sender] >= 50, "Insufficient creditworthiness.");
 
-        // Decide interest rate based on loan amount and duration
-        uint256 interestRate;
-        if (_amount <= 1000 && _duration <= 12) {
-            interestRate = 5;
-        } else if (_amount <= 10000 && _duration <= 24) {
-            interestRate = 7;
-        } else {
-            interestRate = 10;
-        }
+        uint256 interestRate = interestRates[_amount][_duration];
 
         // Calculate interest
         uint256 interest = _amount * interestRate * _duration / 100;
-
+        
         // Store loan details
-        Loan memory newLoan = Loan(loanCounter, _amount, _duration, interestRate, interest, false);
-        loans[msg.sender].push(newLoan);
-        loanCounter++;
+        Loan memory newLoan = Loan(generateLoanId(_amount), _amount, _duration, interestRate, interest, false, false);
+        loans[msg.sender][generateLoanId(_amount)] = newLoan;
+        loancounts[msg.sender]++;
+
+        // Emit event to log loan application
+        emit LoanApplied(msg.sender, generateLoanId(_amount));
     }
 
     // Function to approve a loan
     function approveLoan(address payable _borrower, uint256 _loanId) public {
         // Check if the loan with the specified ID exists for the borrower
-        require(loans[_borrower][_loanId].loanId == _loanId, "Loan not found.");
+        Loan memory loan = loans[_borrower][_loanId];
+        require(loan.loanId == _loanId, "Loan not found.");
         // Check if loan is not approved
-        require(!loans[_borrower][_loanId].approved, "Loan already approved.");
+        require(!loan.approved, "Loan already approved.");
 
         // Check if creditworthiness of the borrower is sufficient
         require(creditworthiness[_borrower] >= 50, "Insufficient creditworthiness.");
 
         // Transfer the loan amount to the borrower
-        require(contractBalance >= loans[_borrower][_loanId].amount, "Insufficient funds in contract.");
-        // require(_borrower.send(loans[_borrower][_loanId].amount), "Transfer failed.");
-        contractBalance -= loans[_borrower][_loanId].amount;
+        require(contractBalance >= loan.amount, "Insufficient funds in contract.");
+        // Check the success of the transfer
+        bool transferSuccessful = _borrower.send(loan.amount);
+        require(transferSuccessful, "Transfer failed.");
+        contractBalance -= loan.amount;
 
         // Approve the loan
-        loans[_borrower][_loanId].approved = true;
+        loan.approved = true;
+
+        // Emit event to log loan approval
+        emit LoanApproved(msg.sender, generateLoanId(loan.amount));
 
         // improve the creditworthiness
         creditworthiness[_borrower] += 10;
     }
 
-    function repay(address payable _borrower, uint256 _loanId, uint256 _amount) public payable{
+
+    function repay(address payable _borrower, uint256 _loanId, uint256 _amount) public payable {
         // Check if the loan with the specified ID exists for the borrower
         require(loans[_borrower][_loanId].loanId == _loanId, "Loan not found.");
 
@@ -83,8 +102,26 @@ contract LoanApplication {
 
         // Update the amount repaid for the loan
         loans[_borrower][_loanId].amount = 0;
+        loans[_borrower][_loanId].repaid = true; // set the loan as repaid
         contractBalance += _amount;
-       
-}
+        
+        // Emit event to log loan approval
+        emit LoanRepaid(msg.sender, generateLoanId(_amount));
+    }
+
+
+    // Function to set interest rates
+    function setInterestRate(uint256 _amount, uint256 _duration, uint256 _interestRate) public {
+        require(msg.sender == address(this), "Only contract owner can set interest rates.");
+        interestRates[_amount][_duration] = _interestRate;
+        
+        emit InterestRateSet(_amount, _duration, _interestRate);
+    }
+
+    // Default function to revert all unknown transactions
+    fallback () external payable {
+        // Your code here, for example to revert unknown transactions
+        revert("This contract does not accept ether.");
+    }
 
 }
